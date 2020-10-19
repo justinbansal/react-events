9ad7c48671ed0d6f09fc4a7404e940090dbf0c90
Justin Bansal <justin@outofthesandbox.com>
2020-10-18 20:02:34 -0400

    Project setup

---
+++ src/components/EventBuilder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class EventBuilder extends React.Component {
+  render() {
+    return (
+      <h1>Event Builder</h1>
+    )
+  }
+}
+
+export default EventBuilder;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import EventBuilder from './components/EventBuilder';
+
+render(<EventBuilder />, document.querySelector('#root'));
+
+
+
