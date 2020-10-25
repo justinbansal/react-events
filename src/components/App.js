9edd2534e2e6643a172422e65bf44ecead593bfc
Justin Bansal <justin@outofthesandbox.com>
2020-10-25 14:23:27 -0400

    Remove firebase db auth logic

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,6 @@ import EventBuilder from './EventBuilder';
 import RegisteredEvent from './RegisteredEvent';
 import Login from './Login';
 import sampleEvents from '../sampleEvents';
-import firebase from 'firebase';
-import base, { firebaseApp } from '../base';
 
 class App extends React.Component {
   state = {
@@ -17,49 +15,15 @@ class App extends React.Component {
 
   componentDidMount = () => {
     console.log('MOUNTED');
-
-    this.EventRef = base.syncState('react-events-b1478/events', {
-      context: this,
-      state: 'events',
-      asArray: true
-    });
-
-    this.UsersRef= base.syncState('react-events-b1478/users', {
-      context: this,
-      state: 'users'
-    });
-
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.authHandler({ user });
-      }
-    })
   }
 
-  // componentDidUpdate = () => {
-  //   console.log('UPDATED!')
-  //   const events = this.state.events.slice();
+  componentDidUpdate = () => {
+    console.log('UPDATED!')
 
-  //   // compare id of items in both arrays
-  //   events.forEach(event => {
-  //     event.available = true;
-  //     this.state.registeredEvents.forEach(registeredEvent => {
-  //       if (event.id === registeredEvent.id) {
-  //         event.available = false;
-  //       }
-  //     })
-  //   })
-
-  //   console.log(events);
-
-  //   this.setState({
-  //     events: events
-  //   })
-  // }
+  }
 
   componentWillUnmount = () => {
-    base.removeBinding(this.EventRef);
-    base.removeBinding(this.UsersRef);
+    console.log('UNMOUNT!')
   }
 
   addEvent = (event) => {
@@ -69,13 +33,8 @@ class App extends React.Component {
       events: events
     })
 
-    // Update users object
-    // Find user
-    // Push event object to created events array
-
     const users = {...this.state.users};
     users[this.state.uid].created.push(event);
-    console.log(users);
   }
 
   loadSampleEvents = () => {
@@ -125,54 +84,9 @@ class App extends React.Component {
     })
   }
 
-  authHandler = (authData) => {
-
-    // Look up current user
-    // const users = await base.fetch(`react-events-b1478/users/${authData.user.uid}`, { context: this}) // returns promise
-    // console.log(users);
-
-    // if (!users[this.state.uid]) {
-    //   // Save it
-    //   await base.post('react-events-b1478/users/user', {
-    //     data: authData.user.uid
-    //   })
-    // }
-    const users = {};
-    users[authData.user.uid] = {
-      created: [],
-      registered: []
-    }
-
-    console.log(users);
-
-    this.setState({
-      users: users,
-      uid: authData.user.uid
-    })
-  }
-
-  authenticate = (provider) => {
-    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
-    firebaseApp.auth().signInWithPopup(authProvider).then(this.authHandler);
-  }
-
-  logout = async () => {
-    await firebase.auth().signOut();
-
-    this.setState({
-      uid: null,
-    })
-  }
 
   render() {
     const logout = <button onClick={this.logout}>Logout!</button>
-
-    console.log(this.state.users[this.state.uid]);
-
-    // Check if they are logged in and if the user has been added
-    if (!this.state.uid) {
-      return <Login authenticate={this.authenticate}/>
-    }
 
     return (
       <div className="react-events">
@@ -193,11 +107,11 @@ class App extends React.Component {
         <div className="event-details">
           <h2>My Events</h2>
           <ul className="registered-events">
-            {this.state.users[this.state.uid].registered.map((element, index) => (
+            {this.state.registeredEvents.map((element, index) => (
               <RegisteredEvent
                 key={index}
                 index={index}
-                details={this.state.users[this.state.uid].registered[index]}
+                details={this.state.registeredEvents[index]}
                 formatMoney={this.formatMoney}
                 removeEvent={this.removeEvent}
               />
